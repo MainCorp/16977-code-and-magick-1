6fe4cd24a8a056da7f971939edc51087ba9e0682
Litnat Corporation <Litnat Corporation>
2017-08-13 19:28:30 +0300

    Изменил способ возвращения максимального значения времени

--- js/stat.js
+++ js/stat.js
@@ -1,22 +1,22 @@
 'use strict';
 
 function randomValue(min, max) {
-  return Math.rand(max - min) + min;
+  return Math.random(max - min) + min;
 }
 
-function calculateHeight(time, times) {
+function calculateHeight(time, maxTime) {
   var maxHeightColumn = 100;
-  var columnHeight = time / times[times.length - 1] * maxHeightColumn;
+  var columnHeight = time / maxTime * maxHeightColumn;
 
   return columnHeight;
 }
 
-function createStatistic(ctx, name, time, indent, times) {
+function createStatistic(ctx, name, time, indent, maxTime) {
   time = Math.ceil(time);
 
-  ctx.fillStyle = name === 'Вы' ? ctx.fillStyle = 'rgba(255, 0, 0, 1)' : ctx.fillStyle = 'rgba(0, 0, 255, ' + randomInteger(0.25, 1) + ')';
-
-  ctx.fillRect(170 + indent, 125, 40, calculateHeight(time, times));
+  ctx.fillStyle = name === 'Вы' ? ctx.fillStyle = 'rgba(255, 0, 0, 1)' : ctx.fillStyle = 'rgba(0, 0, 255, ' + randomValue(0.25, 1) + ')';
+  console.log(maxTime);
+  ctx.fillRect(170 + indent, 125, 40, calculateHeight(time, maxTime));
   ctx.fillStyle = 'rgba(0, 0, 0, 1)';
   ctx.fillText(name, 170 + indent, 245);
   ctx.fillText(time, 170 + indent, 120);
@@ -24,9 +24,7 @@ function createStatistic(ctx, name, time, indent, times) {
 
 window.renderStatistics = function (ctx, names, times) {
   var indent = 0;
-  times = times.sort(function (a, b) {
-    return Math.ceil(a) - Math.ceil(b);
-  });
+  var maxTime = 0;
 
   ctx.fillStyle = 'rgba(0, 0, 0, .7)';
   ctx.fillRect(110, 20, 420, 270);
@@ -39,8 +37,14 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(255, 255, 255, 0.1)';
   ctx.fillRect(130, 100, 360, 150);
 
+  for (var i = 0; i < times.length; i++) {
+    if (times[i] > maxTime) {
+      maxTime = times[i];
+    }
+  }
+
   for (var i = 0; i < names.length; i++) {
-    createStatistic(ctx, names[i], times[i], indent, times);
+    createStatistic(ctx, names[i], times[i], indent, maxTime);
     indent += 80;
   }
 };
